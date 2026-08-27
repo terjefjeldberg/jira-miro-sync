6d2e9e262fa8e035c4d9712ac2309ee5e1bb2c1d
terjefjeldberg <terje@rendra.io>
2026-08-27 13:35:56 +0200

    Trigger Incoming creation for newly created backlog issues

--- src/production-entry.js
+++ src/production-entry.js
@@ -1,5 +1,5 @@
 import reporterWorker from "./reporter-panel-diagnostic-entry.js";
-import previewWorker from "./incoming-card-entry.js";
+import previewWorker from "./incoming-create-any-status-entry.js";
 
 function responseWithText(original, text) {
   const headers = new Headers(original.headers);
